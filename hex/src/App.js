9b8ac3253fb3b1a3731d1c1052a12ba67523fd09
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2024-11-12 10:46:43 -0600

    final random component added

--- hex/src/App.js
+++ hex/src/App.js
@@ -1,11 +1,10 @@
 
 import './App.css';
+import RandomColor from './components/random-color';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+   <RandomColor/>
   );
 }
 
